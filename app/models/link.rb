4d1c70caf092d8cc717d8d9ccdf35b51ffef500a
Mannat <mannattuli13@gmail.com>
2022-01-19 19:28:05 +0530

    E

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
     validates_presence_of :url
+    validates_presence_of :slug
+    validates :slug, format: { with:  /\A[a-zA-Z0-9 ]+\z/ }
+    validates :slug, format: { without: /\s/, message: "slug can't contain spaces" }
     validates :url, format: URI::regexp(%w[http https])
     validates_uniqueness_of :slug
     validates_length_of :url, within: 3..255, on: :create, message: "too long or short"
